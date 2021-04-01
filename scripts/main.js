55d8da105cdd2942570b97478e90bc3dcc5aac9f
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-04-01 09:33:03 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+// LOAD DATA OF HEROES
 const loadData = heroes => {
     console.log(heroes)
 };
 
+// PART OF SEARCH SYSTEM
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 
@@ -22,6 +24,7 @@ const searchStates = async searchText => {
     outputHtml(matches);
 };
 
+// PRINT THE MATCHES OF SEARCH IN THE HTML
 const outputHtml = matches => {
     if (matches.length > 0) {
         const html = matches.map(match => `
@@ -35,7 +38,9 @@ const outputHtml = matches => {
 
 search.addEventListener('input', () => searchStates(search.value));
 
+// SORT SYSTEM
 
+// DISPLAYING JSON DATA
 fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json').then(res => {
     res.json().then(data => {
         console.log(data);
@@ -43,7 +48,7 @@ fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json').then
             var temp = "";
             data.forEach((u) => {
                 temp += "<tr>";
-                temp += "<td>" + "<img src="+ u.images.xs+">" + "</td>";
+                temp += "<td>" + "<img src=" + u.images.xs + ">" + "</td>";
                 temp += "<td>" + u.name + "</td>";
                 temp += "<td>" + u.biography.fullName + "</td>";
                 temp += "<td>" + u.powerstats.intelligence + "</td>";
