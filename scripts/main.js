41a7a4cae8ae278ceffdb19640ffbe00c7f8b273
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-04-01 13:42:54 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,11 +25,37 @@ const loadCharacters = async() => {
     try {
         const res = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json');
         hpCharacters = await res.json();
+        hpCharacters.then(data => {
+            console.log(data);
+            if (data.length > 0) {
+                var temp = "";
+                data.forEach((u) => {
+                    temp += "<tr>";
+                    temp += "<td>" + "<img src=" + u.images.xs + ">" + "</td>";
+                    temp += "<td>" + u.name + "</td>";
+                    temp += "<td>" + u.biography.fullName + "</td>";
+                    temp += "<td>" + u.powerstats.intelligence + "</td>";
+                    temp += "<td>" + u.powerstats.strength + "</td>";
+                    temp += "<td>" + u.powerstats.speed + "</td>";
+                    temp += "<td>" + u.powerstats.durability + "</td>";
+                    temp += "<td>" + u.powerstats.power + "</td>";
+                    temp += "<td>" + u.powerstats.combat + "</td>";
+                    temp += "<td>" + u.appearance.race + "</td>";
+                    temp += "<td>" + u.appearance.gender + "</td>";
+                    temp += "<td>" + u.appearance.height + "</td>";
+                    temp += "<td>" + u.appearance.weight + "</td>";
+                    temp += "<td>" + u.biography.placeOfBirth + "</td>";
+                    temp += "<td>" + u.biography.alignment + "</td></tr>";
+                });
+                document.getElementById("data").innerHTML = temp;
+            }
+        });
         displayCharacters(hpCharacters);
     } catch (err) {
         console.error(err);
-    }
+    };
 };
+
 
 const displayCharacters = (characters) => {
     const htmlString = characters
@@ -49,30 +75,3 @@ loadCharacters();
 // SORT SYSTEM
 
 // DISPLAYING JSON DATA
-fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json').then(res => {
-    res.json().then(data => {
-        console.log(data);
-        if (data.length > 0) {
-            var temp = "";
-            data.forEach((u) => {
-                temp += "<tr>";
-                temp += "<td>" + "<img src=" + u.images.xs + ">" + "</td>";
-                temp += "<td>" + u.name + "</td>";
-                temp += "<td>" + u.biography.fullName + "</td>";
-                temp += "<td>" + u.powerstats.intelligence + "</td>";
-                temp += "<td>" + u.powerstats.strength + "</td>";
-                temp += "<td>" + u.powerstats.speed + "</td>";
-                temp += "<td>" + u.powerstats.durability + "</td>";
-                temp += "<td>" + u.powerstats.power + "</td>";
-                temp += "<td>" + u.powerstats.combat + "</td>";
-                temp += "<td>" + u.appearance.race + "</td>";
-                temp += "<td>" + u.appearance.gender + "</td>";
-                temp += "<td>" + u.appearance.height + "</td>";
-                temp += "<td>" + u.appearance.weight + "</td>";
-                temp += "<td>" + u.biography.placeOfBirth + "</td>";
-                temp += "<td>" + u.biography.alignment + "</td></tr>";
-            })
-            document.getElementById("data").innerHTML = temp;
-        }
-    })
-})
